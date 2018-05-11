4e5be688cc94881902ff3eacf119bc24bd601037
sandmm <1424193282@qq.com>
2018-05-11 05:03:47 -0400

    字符串分离提取函数

---
+++ control/strtok.c
@@ -0,0 +1,34 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+
+int main()
+{
+	char str[]= "ls -l";
+	char *ptr;
+	char *p;
+	printf("%s\n",str);
+	ptr = strtok_r(str," ",&p);
+	while(ptr != NULL)
+	{
+		printf("str = %s\n",str);
+		printf("ptr = %s\n",ptr);
+	    ptr = strtok_r(NULL," ",&p);
+	}
+	return 0;
+}
+//int main()  
+//{  
+//	char str[]="ab,cd,ef";  
+//	char *ptr;  
+//	char *p;  
+//	printf("before strtok:  str=%s\n",str);  
+//	printf("begin:\n");  
+//	ptr = strtok_r(str, ",", &p);  
+//	while(ptr != NULL){  
+//		printf("str=%s\n",str);  
+//		printf("ptr=%s\n",ptr);  
+//		ptr = strtok_r(NULL, ",", &p);  
+//	}  
+//	return 0;  
+//}
