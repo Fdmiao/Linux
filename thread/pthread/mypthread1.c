85d4898095468d0f67b7f980c5a57a25ebd99470
sandm <1424193282@qq.com>
2018-05-20 08:51:15 +0800

    测试创建的线程的tid

--- thread/pthread/mypthread1.c
+++ thread/pthread/mypthread1.c
@@ -5,6 +5,13 @@
 void* pthread1(void* arg)
 {
     printf("i am phread1\n");
+    int i = 10;
+
+    while(i--)
+    {
+        sleep(1);
+    }
+
     return (void*)1;
 }
 
@@ -26,22 +33,28 @@ void* pthread3(void* arg)
 }
 int main()
 {
-    pthread_t tid;
+    pthread_t tid1,tid2,tid3;
     void* ret;
     
+    //如果一个线程退出后，之后创建的线程会在之前退出线程的内存空间上创建结构体
+    //所以，每次输出的tid值相同，按下面注释方式创建的tid均相同
+    //按下面没有注释的方式创建的线程tid不同
     printf("main tid : %x\n",pthread_self());
     
-    pthread_create(&tid,NULL,pthread1,NULL);
-    pthread_join(tid,&ret);
-    printf("pthread1 tid:%x return code: %d\n",tid,(int)ret);
+    pthread_create(&tid1,NULL,pthread1,NULL);
+    //pthread_join(tid1,&ret);
+    printf("pthread1 tid:%x return code: %d\n",tid1,(int)ret);
 
-    pthread_create(&tid,NULL,pthread2,NULL);
-    pthread_join(tid,&ret);
-    printf("pthread1 tid:%x return code: %d\n",tid,(int)ret);
+    pthread_create(&tid2,NULL,pthread2,NULL);
+    //pthread_join(tid2,&ret);
+    printf("pthread1 tid:%x return code: %d\n",tid2,(int)ret);
 
-    pthread_create(&tid,NULL,pthread3,NULL);
-    pthread_cancel(tid);
-    pthread_join(tid,&ret);
-    printf("pthread3 id:%x return code: %d\n",tid,(int)ret);
+    pthread_create(&tid3,NULL,pthread3,NULL);
+    pthread_cancel(tid3);
+    //pthread_join(tid3,&ret);
+    printf("pthread3 id:%x return code: %d\n",tid3,(int)ret);
+    pthread_join(tid1,&ret);
+    pthread_join(tid2,&ret);
+    pthread_join(tid3,&ret);
     return 0;
 }
