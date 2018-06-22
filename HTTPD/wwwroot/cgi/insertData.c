0f1dc513bbf3fe36a4cd1b258510b468da3efa70
sandm <1424193282@qq.com>
2018-06-22 22:15:40 +0800

    HTTP服务器

--- HTTPD/wwwroot/cgi/insertData.c
+++ HTTPD/wwwroot/cgi/insertData.c
@@ -1,8 +1,71 @@
 #include<stdio.h>
 #include<mysql.h>
+#include<string.h>
+#include<stdlib.h>
 
+void insert_data(char* name,char* sex,char* phone)
+{
+    MYSQL *mysql_fd = mysql_init(NULL);
+    //一个IP向不同的端口号发送请求，此时可以在防火墙的角度屏蔽他
+    if(mysql_real_connect(mysql_fd,"127.0.0.1","root","yan21188*","31_class",3306,NULL,0) == NULL)
+    {
+        printf("connect failed!\n");
+        return;
+    }
+    printf("connect success!\n");
+    char sql[1024];
+    sprintf(sql,"insert into student_info(name,sex,phone) values(\"%s\",\"%s\",\"%s\")",name,sex,phone);
+    printf("sql:%s",sql);
+    //const char* sql = "insert into student_info(name,sex,phone) values(\"yanxuechun\",\"man\",\"18710600739\")";
+
+    mysql_query(mysql_fd,sql);
+
+
+    mysql_close(mysql_fd);
+}
+
+
+//该函数为cgi程序
 int main()
 {
-    printf("mysql version:%s\n",mysql_get_client_info());
+    //printf("mysql version:%s\n",mysql_get_client_info());
+    
+    char data[1024];
+    if(getenv("METHOD"))
+    {
+        if(strcasecmp("GET",getenv("METHON")) == 0)
+        {
+            strcpy(data,getenv("QUERY_STRING"));
+        }
+        else
+        {
+            int content_length = atoi(getenv("CONTENT_LENGTH"));
+            int i = 0;
+            for(;i < content_length;i++)
+            {
+                read(0,data+i,1);
+            }
+            data[i] = 0;
+        }
+    }
+    printf("arg: %s\n",data);
+    //name=""&sex=""&phone=""
+ 
+    //为什么用指针，不用数组
+    char *name;
+    char *sex;
+    char *phone;
+    
+    strtok(data,"=&");
+    name = strtok(NULL,"=&");
+    strtok(NULL,"=&");
+    sex = strtok(NULL,"=&");
+    strtok(NULL,"=&");
+    phone = strtok(NULL,"=&");
+    
+    //sscanf(data,"name=%s&sex=%s&phone=%s",name,sex,phone);
+
+    insert_data(name,sex,phone);
+    
     return 0;
 }
--- HTTPD/wwwroot/cgi/selectData.c
+++ HTTPD/wwwroot/cgi/selectData.c
@@ -1,6 +1,46 @@
 #include<stdio.h>
+#include<mysql.h>
 
 int main()
 {
+    MYSQL *mysql_fd = mysql_init(NULL);
+    //一个IP向不同的端口号发送请求，此时可以在防火墙的角度屏蔽他
+    if(mysql_real_connect(mysql_fd,"127.0.0.1","root","yan21188*","31_class",3306,NULL,0) == NULL)
+    {
+        printf("connect failed!\n");
+        return;
+    }
+    printf("connect success!\n");
+    char sql[1024];
+    sprintf(sql,"select* from student_info");
+    mysql_query(mysql_fd,sql);
+    //得到查询结果
+    MYSQL_RES* res = mysql_store_result(mysql_fd);
+  //表中一共几行
+    int row = mysql_num_rows(res);
+    //表中一共几列
+    int col = mysql_num_fields(res);
+    //得到列名
+    MYSQL_FIELD* field = mysql_fetch_fields(res);
+    //打印列名
+    int i = 0;
+    for(;i < col;i++)
+    {
+        printf("%s\t",field[i].name);
+    }
+    printf("\n");
+
+    for(i = 0;i < row;i++)
+    {
+        //获得每行的内容
+        MYSQL_ROW rowdata = mysql_fetch_row(res);
+        int j = 0;
+        for(;j < col;j++)
+        {
+            printf("%s\t",rowdata[j]);
+        }
+        printf("\n");
+    }
+    mysql_close(mysql_fd);
     return 0;
 }
